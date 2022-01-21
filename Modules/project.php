c1306d8df44b324806106c9150a5529af9f3363d
Suliman <302938475@student.rocmondriaan.nl>
2022-01-21 12:46:30 +0100

    klaar

--- Modules/project.php
+++ Modules/project.php
@@ -12,11 +12,10 @@ function getProject(){
 }
 
 
-
 function getProject1(){
 
     global $pdo;
-    $query = $pdo->prepare("SELECT * FROM project LIMIT 2");
+    $query = $pdo->prepare("SELECT * FROM project");
     $query->execute();
     $request = $query->fetchAll(PDO::FETCH_CLASS, 'Project');
 
